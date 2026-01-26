f88eae18bc76727f823549a8b6b17eafcb24631d
kathrinpatel <mp@patel-design.de>
2026-01-26 17:37:03 +0100

    Update page.tsx

--- app/arbeitsproben/page.tsx
+++ app/arbeitsproben/page.tsx
@@ -1,1 +1,31 @@
+import { workItems } from "../../content/arbeitsproben";
 
+export default function ArbeitsprobenPage() {
+  return (
+    <main style={{ maxWidth: 1100, margin: "0 auto", padding: "48px 24px" }}>
+      <h1>Arbeitsproben</h1>
+
+      {workItems.map((item) => (
+        <div key={item.id} style={{ marginBottom: 32 }}>
+          <strong>{item.title}</strong>
+          <br />
+          {item.client}
+          <br />
+          {item.kind}
+
+          {item.downloads && (
+            <ul>
+              {item.downloads.map((d) => (
+                <li key={d.href}>
+                  <a href={d.href} target="_blank">
+                    {d.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      ))}
+    </main>
+  );
+}
